0fefc4c63262ca97822a6c5f41ee86ebdff36578
Justin Mitchel <codingforentrepreneurs@gmail.com>
2014-11-13 14:52:57 -0800

    Ajax Testing

--- djgap/djgap/urls.py
+++ djgap/djgap/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 from tastypie.api import Api
 
@@ -12,6 +13,7 @@ v1_api.register(PostingResource())
 
 urlpatterns = patterns('',
     # Examples:
+    url(r'testing/', TemplateView.as_view(template_name="ajax_testing.html")),
     url(r'^api/', include(v1_api.urls)),
     url(r'^$', 'djgap.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
